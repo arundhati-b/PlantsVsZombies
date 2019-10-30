19424d0730c6307ab7f56cee2736005866f6f6b9
Arundhati Bhattacharya <barundhati19@gmail.com>
2019-10-31 00:08:23 +0530

    GUI for Screens created

---
+++ src/sample/MainScreen.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class MainScreen {
+//    final play;
+//    final load;
+//    final leaderBoard;
+//    final exit;
+    public void press(ActionEvent e)
+    {
+        System.out.println("Ok");
+    }
+}
